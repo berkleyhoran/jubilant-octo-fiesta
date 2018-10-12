7fe97c34c59d0993e4595597e12a728cb0430dbf
Berkley Horan <horans11@gmail.com>
2018-10-12 09:21:23 -0600

    added javascript to the todo cards

---
+++ script.js
@@ -0,0 +1,16 @@
+function addtask(){
+var card = `<div class='card'></i>
+    
+    <p class='todo'>taking the trash out</p>
+  
+    <i class='fas fa-times-circle' id='delete'>`;
+    document.getElementById("cardmain").innerHTML += card;
+
+}    
+    
+  
+
+
+
+    
+    
