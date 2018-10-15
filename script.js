336160483ef50a1287991ef4c18d04850bc71989
Berkley Horan <horans11@gmail.com>
2018-10-15 09:23:04 -0600

    added jquery and add

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-function addtask(){
-var card = `<div class='card'></i>
+window.onload = function() {
+    cardID = 1;
     
-    <p class='todo'>taking the trash out</p>
-  
-    <i class='fas fa-times-circle' id='delete'>`;
-    document.getElementById("cardmain").innerHTML += card;
+   }
+function addtask(){
 
-}    
+    
+    
+    $("#cardmain").append(`<div class='card'></i>
+    
+    <div id="submit" onclick="inlay(this);"><i class="fas fa-check" id="checksub"></i></div><p class='todo'><input id="tasktitle" class="text"></input></p>
+  
+    <span style="flex-wrap:nowrap;"></div>`)
+ 
+
+
+} 
+function inlay(element){
+    let task = $(element).parent().find("#tasktitle").val();
+
+    console.log(task);
+
+    let listamount = $("#cardmain").children().length;
+
+    console.log(listamount);
+
+    console.log(element);
+
+    $(element).parent().html(`<h1>${task}</h1>`)
+}
+
+function clearList(){
+
+    $(".card").remove();
+}
+
+
     
   
 
