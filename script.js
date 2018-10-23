829a533dfdc2327c0eb1be30b502476d2553f724
Berkley Horan <horans11@gmail.com>
2018-10-23 09:48:21 -0600

    starting  forming lists into arrays

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-window.onload = function() {
-    cardID = 1;
-    
-   }
+var lists = {
+     
+
+        
+       
+
+
+
+
+
+};
+window.onload = function(){
+    var listid = 0;
+}
+$("#cardmain").sortable({
+    handle:"#draggable"
+})
+
 function addtask(){
 
     
@@ -9,7 +23,16 @@ function addtask(){
     $("#cardmain").append(`<div class='card'></i>
     
     <span id="cardspace"><div id="submit" onclick="inlay(this);"><i class="fas fa-check" id="checksub"></i></div>
-    <input id="tasktitle" class="text"></input><div class="blacksquare"><input type="checkbox" class="checkmark"></div></div></span>
+
+    <input id="tasktitle" class="text"></input>
+    
+    <div class="blacksquare">
+    
+    <input type="checkbox" class="checkmark" onclick="selectcard(this);">
+    
+     <i class="fas fa-times-circle" onmouseover="this.parentNode.style.background='red'" onmouseleave="selectcard(this)"  onclick="removecard(this);"></i>
+    
+    <i class="fas fa-arrows-alt" id="draggable"></i></div>
   
     `)
  
@@ -27,12 +50,72 @@ function inlay(element){
 
     console.log(element);
 
-    $(element).parent().html(`<h1>${task}</h1><div class="blacksquare"><input type="checkbox" class="checkmark"></div></div>`)
+    $(element).parent().html(`<h1>${task}</h1><div class="blacksquare">
+    
+    <input type="checkbox" class="checkmark" onclick="selectcard(this);">
+    
+    <i class="fas fa-times-circle" onmouseover="this.parentNode.style.background='red'" onmouseleave="selectcard(this);" onclick="removecard(this);></i>
+    
+    <i class="fas fa-arrows-alt" id="draggable"></i>
+    
+    </div>`)
 }
 
 function clearList(){
-
     $(".card").remove();
+}
+
+function removecard(thiscard){
+    console.log(thiscard);
+    $(thiscard).parent().parent().parent().remove()
+}
+
+function selectcard(selectedcard){
+    console.log(selectedcard);
+    let checked = selectedcard.checked;
+    if(checked == true){
+        console.log(checked);
+        $(selectedcard).parent().css("background-color","#a9cea1")
+        
+       }
+    if (checked == false){
+        console.log(checked);
+        $(selectedcard).parent().css("background-color","#252C3F")
+        
+    }
+    if (checked == null){
+        console.log(checked);
+        $(selectedcard).parent().css("background-color","#a9cea1")
+        if (checked == false){
+            $(selectedcard).parent().css("background-color","#252C3F")
+        }
+        
+    }
+    
+}
+
+function deleteselected(){
+    
+}
+
+function createlist(){
+
+    var newlist = {
+        id: listid++
+        name: listname
+        
+    }
+    list.push();
+
+
+    var listname = $("#listname").val();
+
+    
+    
+    if(listname.length > 2){
+    $("#matnav").append(`<div class="navelem">${listname}</div>`)
+    }
+
 }
 
 
